11629d585771d4837f6d640b50429a113bdc4635
Paulo Osawa <nhnpublicidade@gmail.com>
2022-04-22 19:54:16 -0300

    Fixing debug and response messages on produtos-controller file

--- controllers/produtos-controller.js
+++ controllers/produtos-controller.js
@@ -1,6 +1,5 @@
-const mysqlb = require('../mysql');
+const mysql = require('../mysql');
 
-const tbs = ['prod_categs', 'prods', 'prod_imgs', 'vw_prodsb']    
 const tbbs = {
   prod_categs : ['id_categ', 'priority', 'nome_categ'] ,
   prods       : ['id_prod', 'fk_id_categ', 'nome_prod', 'titulo', 'descricao', 'preco'] ,
@@ -14,7 +13,7 @@ const categorias = []
 const inigetcategs = async () => {
   const query = 'SELECT * FROM prod_categs;';
   try {
-    const result =  await mysqlb.execute(query);    
+    const result =  await mysql.execute(query);    
     return result;    
   } catch (error) {
     return { erro: error }
@@ -37,13 +36,13 @@ exports.getCategs = async (req, res) => {
   if (req.query.from) {
     try {
       const response = await inigetcategs()
-      console.log('from = ', req.query );
+      console.log('requesting from DB');
       return res.status(200).send({ response: response }) 
     } catch (error) {
       return res.status(500).send({ erro: error})
     }    
   } else if (categorias.length) { 
-    console.log('from = const arey' );
+    console.log('requesting categories from populated Array categorias as cached response' );
     return res.status(200).send({ response: categorias }) 
   } else {
     return res.status(500).send({ erro: 'error'});
@@ -51,13 +50,13 @@ exports.getCategs = async (req, res) => {
 }
 // ---------------------------------------
 exports.addCateg = async (req, res, next) => {
-  if (faltaCampos(['priority', 'nome_categ'],req.body)) { return res.status(500).send({ erro:'faltaCampos'}) }
+  if (faltaCampos(['priority', 'nome_categ'],req.body)) { return res.status(500).send({ erro:'faltando Campos!'}) }
   const { priority, nome_categ } = req.body;
   const query = 'INSERT INTO prod_categs(priority, nome_categ) values (?,?);';
   const params = [priority, nome_categ]
   try {
-    const result =  await mysqlb.execute(query, params);    
-    if (result.affectedRows < 1) { return res.status(401).send({ erro: "não Encontrado!" }) }
+    const result =  await mysql.execute(query, params);    
+    if (result.affectedRows < 1) { return res.status(401).send({ erro: "Erro ao adicionar produto!" }) }
     const response = { id_categ: result.insertId, priority: priority, nome_categ: nome_categ } 
     categorias.push(response)
     return res.status(201).send({ 
@@ -66,17 +65,16 @@ exports.addCateg = async (req, res, next) => {
   } catch (error) {
     return res.status(500).send({ erro: error})
   }
-  // return res.status(201).send({ mensagem: 'addCategx', nome: req.body.nome })
 }
 // ---------------------------------------
 exports.upCateg = async (req, res, next) => {
-  if (faltaCampos(tbbs.prod_categs, req.body)) { return res.status(500).send({ erro:'faltaCampos'}) }
+  if (faltaCampos(tbbs.prod_categs, req.body)) { return res.status(500).send({ erro:'faltando Campos Necessários!'}) }
   const { id_categ, priority, nome_categ } = req.body;
   const query = 'UPDATE prod_categs SET priority = ?, nome_categ = ? WHERE id_categ = ?;';
   const params = [priority, nome_categ, id_categ]
   try {
-    const result =  await mysqlb.execute(query, params);    
-    if (result.affectedRows < 1) { return res.status(401).send({ erro: "não Encontrado!" }) }
+    const result =  await mysql.execute(query, params);    
+    if (result.affectedRows < 1) { return res.status(401).send({ erro: "Erro ao atualizar Categoria!" }) }
     const response = { id_categ: id_categ, priority: priority, nome_categ: nome_categ } 
 
     categorias.find((categ, idx) => {
@@ -95,11 +93,11 @@ exports.upCateg = async (req, res, next) => {
 }
 // ---------------------------------------
 exports.delCateg = async (req, res, next) => {
-  if (!('id_categ' in req.body)) { return res.status(500).send({ erro:'faltaCampos'}) }
+  if (!('id_categ' in req.body)) { return res.status(500).send({ erro:'faltando Campo Obrigatório'}) }
   const query = 'DELETE FROM prod_categs WHERE id_categ = ?;';
   try {
-    const result =  await mysqlb.execute(query, [req.body.id_categ]);    
-    if (result.affectedRows < 1) { return res.status(401).send({ erro: "não Encontrado!" }) }
+    const result =  await mysql.execute(query, [req.body.id_categ]);    
+    if (result.affectedRows < 1) { return res.status(401).send({ erro: "Erro ao remover Categoria!" }) }
 
     categorias.find((categ, idx) => {
       if (categ.id_categ === req.body.id_categ) {
@@ -120,8 +118,8 @@ exports.delCateg = async (req, res, next) => {
 exports.getAllprods = async (req, res, next) => {
   const query = 'SELECT * FROM vw_prodsb ORDER BY idcateg, idprod;';
   try {
-    const result =  await mysqlb.execute(query);    
-    if (result.length < 1) { return res.status(401).send({ erro: "não Encontrado!" }) }
+    const result =  await mysql.execute(query);    
+    if (result.length < 1) { return res.status(401).send({ erro: "Sem registro de produtos!" }) }
     return res.status(200).send({ response: result })
   } catch (error) {
     return res.status(500).send({ erro: error})
@@ -130,9 +128,8 @@ exports.getAllprods = async (req, res, next) => {
 
 exports.getProdsCateg = async (req, res, next) => {
   var params = [req.params.categ]
-  console.log('getProdsCateg params = ', params );
   if (categorias.length && !checkincateg(params[0])) { 
-    return res.status(500).send({ erro: 'xxxxxxxx errado'});
+    return res.status(500).send({ erro: 'Categoria não Encontrada!'});
   }
   var fim = '';
   var err = false;
@@ -143,7 +140,7 @@ exports.getProdsCateg = async (req, res, next) => {
         var sign = (cond === 'maior') ? '>' : '<';
         fim = ` AND preco ${sign} ?`; params.push(req.query['valor']);
       } else {
-        return res.status(500).send({ erro: 'preco errado'});
+        return res.status(500).send({ erro: 'Parâmetro errado em preço!'});
       }      
     } else {
       Object.keys(req.query).map((k) => {
@@ -155,11 +152,11 @@ exports.getProdsCateg = async (req, res, next) => {
       });
     }
   }
-  if (err) { return res.status(500).send({ erro: 'sem chave'}) }
+  if (err) { return res.status(500).send({ erro: 'Parâmetro de consulta inexistente!'}) }
   const query = `SELECT * FROM vw_prodsb WHERE categoria = ?${fim};`;
   try {
-    const result =  await mysqlb.execute(query, params);    
-    if (result.length < 1) { return res.status(401).send({ erro: "não Encontrado!" }) }
+    const result =  await mysql.execute(query, params);    
+    if (result.length < 1) { return res.status(401).send({ erro: "Sem produtos na consulta!" }) }
     return res.status(200).send({ response: result })
   } catch (error) {
     return res.status(500).send({ erro: error})
@@ -169,31 +166,32 @@ exports.getProdsCateg = async (req, res, next) => {
 exports.getOneProd = async (req, res, next) => {
   var params = [req.params.categ];
   if (categorias.length && !checkincateg(params[0])) { 
-    return res.status(500).send({ erro: 'xxxxxxxx errado'});
+    return res.status(500).send({ erro: 'Categoria Inexistente!'});
   }
   var fim = '';
   var err = false;
   const filtro = req.params.filtro;
-  console.log(params, ' 1 PRODUTO = ', filtro);
   if (!isNaN(filtro)) {
     fim = ` AND idprod = ?`; params.push(filtro);
   } else if(!req.query || !(filtro === 'preco' || filtro === 'nome')){
-    return res.status(500).send({ erro: 'sem qqqq'});
+    return res.status(500).send({ erro: 'Erro nos parâmetros de consulta!'});
   } else if(filtro === 'nome' && ('nome' in req.query)){
     fim = ` AND produto = ?`; params.push(req.query.nome);
   } else if(filtro === 'preco'){
     var k = Object.keys(req.query)[0];
     var idx = ['menor', 'maior'].indexOf(k);    
-    if(idx < 0) { err = true; return res.status(500).send({ erro: 'sem chave'}); }
+    if(idx < 0) { err = true; return res.status(500).send({ erro: 'Parâmetro errado em preço!'}); }
     idx = (idx === 1) ? '>' : '<';
     fim = ` AND preco ${idx} ?`; params.push(req.query[k]);
   }
-  if (err) { console.log('xxx', ); return res.status(500).send({ erro: 'fim sem chave'}) }
+  if (err) { 
+    return res.status(500).send({ erro: 'Erro nos parâmetros de consulta!'}) 
+  }
 
   const query = `SELECT * FROM vw_prodsb WHERE categoria = ?${fim};`;  
   try {
-    const result =  await mysqlb.execute(query, params);    
-    if (result.length < 1) { return res.status(401).send({ erro: "não Encontrado!" }) }
+    const result =  await mysql.execute(query, params);    
+    if (result.length < 1) { return res.status(401).send({ erro: "Produto não Encontrado!" }) }
     return res.status(200).send({ response: result })
   } catch (error) {
     return res.status(500).send({ erro: error})
@@ -202,15 +200,15 @@ exports.getOneProd = async (req, res, next) => {
 }
 /* --------------------------------------- */
 exports.addProd = async (req, res, next) => {
-  if (faltaCampos(['nome_prod', 'preco'],req.body)) { return res.status(500).send({ erro:'faltaCampos'}) }
+  if (faltaCampos(['nome_prod', 'preco'],req.body)) { return res.status(500).send({ erro:'faltando Campos Obrigatórios!'}) }
   const categ = req.params.categ;
   var query = 'SELECT id_categ FROM prod_categs WHERE nome_categ = ?;';
   var queryparams = [categ];
   var idcateg = false;
   if (!categorias.length){
     try {
-      const result =  await mysqlb.execute(query, queryparams);    
-      if (result.length < 1) { return res.status(401).send({ erro: "id_categ não Encontrado!" }) }
+      const result =  await mysql.execute(query, queryparams);    
+      if (result.length < 1) { return res.status(401).send({ erro: "Categoria não Encontrada!" }) }
       idcateg = result[0]
     } catch (error) { return res.status(500).send({ erro: error}) }
   } else {
@@ -219,14 +217,14 @@ exports.addProd = async (req, res, next) => {
   const { fk_id_categ, nome_prod, titulo, descricao, preco } = req.body;
   if (!idcateg) {
     if (!fk_id_categ) {
-      return res.status(401).send({ erro: "FK não Encontrado!" })       
+      return res.status(401).send({ erro: "Faltando id da categoria!" })       
     } else { idcateg = fk_id_categ }    
   }
   query = 'INSERT INTO prods (fk_id_categ, nome_prod, titulo, descricao, preco) values (?,?,?,?,?);';
   queryparams = [idcateg, nome_prod, titulo, descricao, preco];
   try {
-    const result =  await mysqlb.execute(query, queryparams);    
-    if (result.affectedRows < 1) { return res.status(401).send({ erro: "não Encontrado!" }) }
+    const result =  await mysql.execute(query, queryparams);    
+    if (result.affectedRows < 1) { return res.status(401).send({ erro: "Erro ao Inserir Produto!" }) }
     return res.status(201).send({ 
       msg: `Produto ${nome_prod} Adicionado com Sucesso!`, 
       response: { id_prod: result.insertId, categoria: categ, nome_prod: nome_prod, preco: preco } 
@@ -238,21 +236,21 @@ exports.addProd = async (req, res, next) => {
 /* --------------------------------------- */
 exports.updateProd = async (req, res, next) => {
   if (faltaCampos(['id_prod', 'nome_prod', 'preco'], req.body)) { 
-    return res.status(500).send({ erro:'faltaCampos'});
+    return res.status(500).send({ erro:'faltando Campos Obrigatórios!'});
   }
   const categ = req.params.categ;
-  if (!categorias.length){ return res.status(500).send({ erro: 'erro servidr'}); }
+  if (!categorias.length){ return res.status(500).send({ erro: 'Erro no servidor, Categorias não populadas!'}); }
   const obcat = checkincateg(categ) ?? false; 
   const { id_prod, fk_id_categ, nome_prod, titulo, descricao, preco } = req.body;
   if (!obcat || (fk_id_categ && obcat.id_categ !== fk_id_categ)) {
-    return res.status(500).send({ erro: 'categ errado'});
+    return res.status(500).send({ erro: 'Categoria não Encontrada!'});
   }
   var idcateg = obcat.id_categ
   query = 'UPDATE prods SET fk_id_categ = ?, nome_prod = ?, titulo = ?, descricao = ?, preco = ? WHERE id_prod = ?;';
   queryparams = [ idcateg, nome_prod, titulo, descricao, preco, id_prod];
   try {
-    const result =  await mysqlb.execute(query, queryparams);    
-    if (result.affectedRows < 1) { return res.status(401).send({ erro: "não Encontrado!" }) }
+    const result =  await mysql.execute(query, queryparams);    
+    if (result.affectedRows < 1) { return res.status(401).send({ erro: "Erro ao Atualizar Produto!" }) }
     return res.status(202).send({ 
       msg: `${result.affectedRows} Produto Atualizado com Sucesso!`,
       response: { id_prod: id_prod, nome_prod: nome_prod, preco: preco } 
@@ -263,13 +261,13 @@ exports.updateProd = async (req, res, next) => {
 }
 /* --------------------------------------- */
 exports.delProd = async (req, res, next) => {
-  if (!('id_prod' in req.body)) { return res.status(500).send({ erro:'faltaCampos'}) }
+  if (!('id_prod' in req.body)) { return res.status(500).send({ erro:'faltando Campo Obrigatório!'}) }
   const categ = req.params.categ;
   const query = 'DELETE FROM prods WHERE fk_id_categ = (SELECT id_categ FROM prod_categs WHERE nome_categ = ?) AND id_prod = ?;';
   const params = [categ, req.body.id_prod]
   try {
-    const result =  await mysqlb.execute(query, params);    
-    if (result.affectedRows < 1) { return res.status(401).send({ erro: "não Encontrado!" }) }
+    const result =  await mysql.execute(query, params);    
+    if (result.affectedRows < 1) { return res.status(401).send({ erro: "Erro ao Remover Produto!" }) }
     return res.status(202).send({ 
       msg: `${result.affectedRows} Produto REMOVIDO com Sucesso!`, response: { results: result.affectedRows }         
     });
