c452228e374765cba01fff42052283b4dfae7926
Paulo Osawa <nhnpublicidade@gmail.com>
2022-04-27 06:45:29 -0300

    Access level functions for admins and user.js refactoring

--- middleware/login.js
+++ middleware/login.js
@@ -1,31 +1,65 @@
 const jwt = require('jsonwebtoken');
 
-const padrao = (token) => {
+/* --------------------------------------- */
+const errMsg = { erro: "Falha de Autenticação!" } 
+/* --------------------------------------- */
+const verifyToken = (token) => {
+  if(!token){ return false; }
+
   try {
-    const decode = jwt.verify(token, process.env.JWT_KEY); return decode;
-  } catch (error) { return { erro: "Falha de Autenticação!"}  }
+    const decode = jwt.verify(token, process.env.JWT_KEY); 
+    return decode;
+  } catch (error) { 
+    return false;
+  }
 }
-
+/* --------------------------------------- */
+const checkRequired = (req, type, adminlvl) => {
+  if (!req.headers.authorization) { return false;  }
+  const ret = verifyToken(req.headers.authorization?.split(' ')[1]);
+  if(ret && (ret?.admlvl || type !== 'adm')){ 
+    if (adminlvl) {
+      if (!ret?.admlvl || adminlvl < ret?.admlvl) { return false; }  
+    }
+    req.usuario = ret; 
+    return true; 
+  } 
+  return false;
+}
+/* --------------------------------------- */
+exports.admlvlOneRequired = (req, res, next) => {
+  if (checkRequired(req, 'adm', 1)) {
+    return next();
+  }
+  return res.status(401).send(errMsg); 
+}
+/* --------------------------------------- */
+exports.admlvlTwoRequired = (req, res, next) => {
+  if (checkRequired(req, 'adm', 2)) {
+    return next();
+  }
+  return res.status(401).send(errMsg); 
+}
+/* --------------------------------------- */
 exports.admrequired = (req, res, next) => {
-  const ret = padrao(req.headers.authorization.split(' ')[1]);
-  if (ret?.erro || !ret?.admlvl) { return res.status(401).send({ erro: ret?.erro ?? 'Not Found'}); }
-  req.usuario = ret;
-  next();
+  if (checkRequired(req, 'adm')) {
+    return next();
+  }
+  return res.status(401).send(errMsg); 
 }
-
+/* --------------------------------------- */
 exports.required = (req, res, next) => {
-  const ret = padrao(req.headers.authorization.split(' ')[1]);
-  if (ret?.erro) { return res.status(401).send(ret); }
-  req.usuario = ret;
-  next();  
+  if (checkRequired(req, 'client')) {
+    return next();
+  }
+  return res.status(401).send(errMsg); 
 }
-
+/* --------------------------------------- */
 exports.optional = (req, res, next) => {
-  const ret = padrao(req.headers.authorization.split(' ')[1]);
-  if (!ret?.erro) { req.usuario = ret; }  
+  checkRequired(req, 'client');
   next();  
 }
-
+/* --------------------------------------- */
 exports.obrigatorio = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
@@ -36,7 +70,7 @@ exports.obrigatorio = (req, res, next) => {
     return res.status(401).send({ erro: "Falha de Autenticação!"})
   }
 }
-
+/* --------------------------------------- */
 exports.opcional = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
--- routers/users.js
+++ routers/users.js
@@ -5,6 +5,8 @@ const mysql = require('../mysql');
 const jwt = require('jsonwebtoken');
 const login = require('../middleware/login');
 
+const tbs = ['clients', 'tbadmins', 'vw_clients', 'vw_admins']
+
 /* --------------------------------------- */
 const faltaCampos = (campos, body) => {
   for (const cp of campos) {
@@ -14,7 +16,7 @@ const faltaCampos = (campos, body) => {
 }
 /* --------------------------------------- */
 const checkUserExist = async (email, adm) => {
-  const tb = (!adm || adm === '') ? 'vw_clients' : 'vw_admins';
+  const tb = (!adm || adm !== 'adm') ? 'vw_clients' : 'vw_admins';
   const query = `SELECT * FROM ${tb} WHERE email = ?;`;
   try {
     const result =  await mysql.execute(query, [email]);
@@ -22,6 +24,49 @@ const checkUserExist = async (email, adm) => {
     return false;
   } catch (error) { return { erro: error } }  
 }
+/* --------------------------------------- */
+const userCadastro = async (req, res, usertype) => {
+  const isAdm = (usertype === 'adm' && req.url.includes('adm'));
+  const cpsVw = isAdm ? [ 'admlvl', 'nome', 'email', 'senha' ] : [ 'nome', 'email', 'senha' ];
+  const errMsg = faltaCampos(cpsVw, req.body);
+  if (errMsg) { return res.status(500).send({ erro: errMsg }) }
+
+  const user = await checkUserExist(req.body.email, usertype);
+  if (user) { return res.status(401).send({ erro: "Usuário já Cadastrado!"}); }
+
+  bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
+    if (errBcrypt) { return res.status(500).send({ erro: errBcrypt }); }
+    const query = isAdm ? 
+    'INSERT INTO tbadmins (lvl_adm, nome_adm, mail_adm, pass_adm) VALUES (?,?,?,?)' :
+    'INSERT INTO clients (nome_cli, mail_cli, pass_cli) VALUES (?,?,?)';
+    const { admlvl, nome, email } = req.body;
+    const params = isAdm ? [admlvl, nome, email, hash] : [nome, email, hash];
+    mysql.execute(query, params).then((result) => { 
+      if (!result || result.affectedRows < 1) { 
+        return res.status(401).send({ erro: "Erro ao Cadastrar Usuário!" }); 
+      }
+      const resp = {
+        msg : 'USUÁRIO CADASTRADO COM SUCESSO!', 
+        id: result.insertId, 
+        nome: nome,
+        usertype : isAdm ? 'admin' : 'client'
+      }
+      return res.status(201).send(resp);
+    }).catch(err => {
+      return res.status(401).send({ erro: "Erro ao Cadastrar Usuário!" });
+    });
+
+  });
+
+}
+/* --------------------------------------- */
+router.post('/admcadastro', (req, res, next) => {
+  return userCadastro(req, res, 'adm');
+});
+/* --------------------------------------- */
+router.post('/cadastro', (req, res, next) => {
+  return userCadastro(req, res, 'client');
+});
 /* --------------------------------------- */
 const userlogin = (req, res, usertype) => {
   const errMsg = faltaCampos([ 'email', 'senha' ], req.body);
@@ -33,7 +78,7 @@ const userlogin = (req, res, usertype) => {
       if(result) { 
         const obuser = { id:'', nome: user.nome, email: req.body.email }
         obuser.id = (usertype === 'adm') ? user?.idadm : user?.iduser ;
-        if(user?.admlvl){ obuser.admlvl = user.admlvl; }         
+        if(user?.admlvl){ obuser.admlvl = user.admlvl; }  
         const token = jwt.sign(obuser, process.env.JWT_KEY, {expiresIn: 300})
         //process.env.JWT_KEY, { expiresIn: "1h" })
         return res.status(201).send({ msg:'USUÁRIO AUTORIZADO', ...obuser, token: token }) 
@@ -42,46 +87,6 @@ const userlogin = (req, res, usertype) => {
     });
   });
 }
-/* --------------------------------------- */
-router.post('/admcadastro', (req, res, next) => {
-  const errMsg = faltaCampos([ 'admlvl', 'nome', 'email', 'senha' ], req.body);
-  if (errMsg) { return res.status(500).send({ erro: errMsg }) }
-  const { admlvl, nome, email, senha } = req.body;
-  checkUserExist(email, 'adm').then((user) => { 
-    if (user) { return res.status(401).send({ erro: "Usuario já Cadastrado!" }); }
-    bcrypt.hash(senha, 10, (errBcrypt, hash) => {
-      if (errBcrypt) { return res.status(500).send({ erro: errBcrypt }); }
-      const query = 'INSERT INTO tbadmins (lvl_adm, nome_adm, mail_adm, pass_adm) VALUES (?,?,?,?)';
-      try {
-        const result =  mysql.execute(query, [admlvl, nome, email, hash]);
-        if (result.affectedRows < 1) { return res.status(401).send({ erro: "Erro ao Cadastrar Admin!" }) }
-        return res.status(201).send({
-          msg : `ADMIN ${nome} CADASTRADO COM SUCESSO!`, nome: nome, id: result.insertId
-        })
-      } catch (error) { return { erro: error } } 
-    });
-  })
-});
-/* --------------------------------------- */
-router.post('/cadastro', (req, res, next) => {
-  const errMsg = faltaCampos([ 'nome', 'email', 'senha' ], req.body);
-  if (errMsg) { return res.status(500).send({ erro: errMsg }) }
-  const { nome, email, senha } = req.body;
-  checkUserExist(email).then((user) => { 
-    if (user) { return res.status(401).send({ erro: "Usuario já Cadastrado!" }); }
-    bcrypt.hash(senha, 10, (errBcrypt, hash) => {
-      if (errBcrypt) { return res.status(500).send({ erro: errBcrypt }); }
-      const query = 'INSERT INTO clients (nome_cli, mail_cli, pass_cli) VALUES (?,?,?)';
-      try {
-        const result =  mysql.execute(query, [nome, email, hash]);
-        if (result.affectedRows < 1) { return res.status(401).send({ erro: "Erro ao Cadastrar Usuário!" }) }
-        return res.status(201).send({
-          msg : `USUÁRIO ${nome} CADASTRADO COM SUCESSO!`, nome: nome, id: result.insertId 
-        })
-      } catch (error) { return { erro: error } } 
-    });
-  })
-});
 /* --------------------------------------- */
 router.post('/admlogin', (req, res, next) => {
   return userlogin(req, res, 'adm');
@@ -92,13 +97,31 @@ router.post('/login', (req, res, next) => {
 });
 /* --------------------------------------- */
 router.get('/needtoken', login.admrequired, (req, res) => {
-  console.log('xxx', );
-  return res.status(200).send({ id: req.usuario.iduser, nome: req.usuario.nome, response : 'Usuário AUTORIZADO' });
+  
+  if (!req.usuario) {return res.status(401).send({ erro : 'Erro de Autenticação' }); } 
+  const usertype = (!req.usuario?.admlvl) ? 'client' : 'admin';
+  const user = { 
+    msg : 'Usuário AUTORIZADO', 
+    id: req.usuario?.id, 
+    nome: req.usuario?.nome,
+    email: req.usuario?.email,
+    usertype: usertype,
+    admlvl: req.usuario?.admlvl 
+  }
+  return res.status(200).send(user);
 });
 /* --------------------------------------- */
 router.get('/revalidateuser', login.obrigatorio, (req, res) => {
+  console.log('revalidateuser', );
   if (!req.usuario) {return res.status(401).send({ erro : 'Erro de Autenticação' }); }
-  const user = { id: req.usuario.iduser, nome: req.usuario.nome }  
+  const usertype = (!req.usuario?.admlvl) ? 'client' : 'admin';
+  const user = { 
+    id: req.usuario?.id, 
+    nome: req.usuario?.nome,
+    email: req.usuario?.email,
+    usertype: usertype,
+    admlvl: req.usuario?.admlvl 
+  }
   return res.status(200).send({ msg : 'Usuário AUTORIZADO', user: user });
 });
 /* --------------------------------------- */
